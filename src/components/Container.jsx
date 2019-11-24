4b3a279a8b3e374c4c1ad991ec0b2aceb4f3d1cd
avi turkewitz <aviturkewitz@gmail.com>
2019-11-24 12:02:42 -0500

    terrible solution

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -12,6 +12,51 @@ function Container() {
     setNumbers(nums)
   }
 
+  const sort = (nums) => {
+    const sorted = split(nums)
+    setNumbers(sorted)
+  }
+
+  const merge = (array1, array2) => {
+    let p1 = 0
+    let p2 = 0
+    const array1Length = array1.length
+    const array2Length = array2.length
+    const merged = []
+    let count = 0
+    while(p1 < array1Length || p2 < array2Length){
+      count++
+      if(array1[p1] === undefined){
+        merged.push(array2[p2])
+        p2++
+      }else if (array2[p2] === undefined) {
+        merged.push(array1[p1])
+        p1++
+      }else if(array1[p1] < array2[p2] ){
+        merged.push(array1[p1])
+        p1++
+      }else{
+        merged.push(array2[p2])
+        p2++
+      }
+      if(count > 15){
+        debugger
+        break
+      }
+    }
+    return merged
+  }
+
+  const split = (nums) => {
+    const length = nums.length
+    if(length > 1){
+      const firstSection = nums.splice(0, Math.floor(length / 2))
+      return merge(split(firstSection), split(nums))
+    }else{
+      return nums
+    }
+  }
+
   const getRandNum = () => (Math.floor(Math.random() * Math.floor(100)))
   return (
     <div>
@@ -23,6 +68,11 @@ function Container() {
           return (<li key={i}>{num}</li>)
         })}
       </ul>
+      {numbers.length > 0 ?
+          <button onClick={() => sort(numbers)}>Sort!</button>
+        :
+          ''
+      }
     </div>
   );
 }
